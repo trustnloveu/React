de0ffc648ebdbc80dab5a291adacecfb527b7aa9
euijin-yang <trustnloveu@gmail.com>
2021-05-30 17:31:01 +0900

    Create simple component, Do export-import it

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>2021년 5월 30일</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>320,400원</div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
