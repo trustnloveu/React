ef4ba8e48dfdb882f17ca0674bfb1fce62db76b8
euijin-yang <trustnloveu@gmail.com>
2021-06-27 15:21:36 +0900

    Add date-labeling component using toLoacleString

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,13 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = (props) => {
-  //   const expenseDate = new Date(2021, 4, 31);
-  //   const expenseTitle = "자동차 보험료";
-  //   const exepnseAmount = "320,400원";
+// Components
+import ExpenseDate from "./ExpenseDate"; // ExpenseDate
 
+const ExpenseItem = (props) => {
   // return
   return (
     <div className="expense-item">
-      <div>{props.data.date.toISOString()}</div>
+      <ExpenseDate date={props.data.date} />
       <div className="expense-item__description">
         <h2>{props.data.title}</h2>
         <div className="expense-item__price">{props.data.amount}</div>
