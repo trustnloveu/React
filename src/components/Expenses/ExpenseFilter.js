da7d973f12791bba89c7999699a433b020b4b2ff
euijin-yang <trustnloveu@gmail.com>
2021-10-05 23:46:38 +0900

    Fix dropdown filter issue

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -3,11 +3,15 @@ import React from "react";
 import "./ExpenseFilter.css";
 
 const ExpenseFilter = ({ selected, onChangeFilter }) => {
+  const dropdownChangeHandler = (event) => {
+    onChangeFilter(event.target.value);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select value={selected} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
