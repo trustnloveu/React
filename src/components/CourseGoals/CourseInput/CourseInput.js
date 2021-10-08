6489e8c517159a251483eb01fb62b4455487e258
ejyang <trustnloveu@gmail.com>
2021-10-08 23:41:29 +0900

    media-query

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -47,14 +47,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")}
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")}
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -65,8 +65,7 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-
-  `;
+`;
 // &.invalid input {
 //   border-color: red;
 //   background: #ffd7d7;
