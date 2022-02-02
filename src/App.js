933fbf99b098018103c18acb765605645bdaf0b1
ejyang <trustnloveu@gmail.com>
2022-02-03 01:51:32 +0900

    Connect Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
 
+import AddMovie from "./components/AddMovie";
 import MoviesList from "./components/MoviesList";
+
 import "./App.css";
 
 function App() {
@@ -35,7 +37,10 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      // const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-test-98851-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) throw new Error("Something went wrong."); // To catch error
 
@@ -72,6 +77,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
