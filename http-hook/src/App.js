d8ec6da787f2004248cc577320b0b7970523e6a6
ejyang <trustnloveu@gmail.com>
2022-02-04 02:00:56 +0900

    simplify code

--- http-hook/src/App.js
+++ http-hook/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -9,29 +9,32 @@ function App() {
   //* State
   const [tasks, setTasks] = useState([]);
 
-  //* Method
-  const transformTasks = useCallback((traskObject) => {
-    const loadedTask = [];
-
-    for (const taskKey in traskObject) {
-      loadedTask.push({ id: taskKey, text: traskObject[taskKey].text });
-    }
-
-    setTasks(loadedTask);
-  }, []);
-
+  //* Handler
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
 
   //* HTTP HOOK
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   //* useEffect
   useEffect(() => {
-    fetchTasks({
-      url: "https://react-test-98851-default-rtdb.firebaseio.com/tasks.json",
-    });
+    const transformTasks = (traskObject) => {
+      const loadedTask = [];
+
+      for (const taskKey in traskObject) {
+        loadedTask.push({ id: taskKey, text: traskObject[taskKey].text });
+      }
+
+      setTasks(loadedTask);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-test-98851-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
   }, [fetchTasks]);
 
   //* return
