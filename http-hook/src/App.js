1adb7d48910cf59d9f08f3135f031c5d787a087d
ejyang <trustnloveu@gmail.com>
2022-02-04 01:46:28 +0900

    http hook for 'GET' request

--- http-hook/src/App.js
+++ http-hook/src/App.js
@@ -1,48 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  //* State
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  //* Method
+  const transformTasks = (traskObject) => {
+    const loadedTask = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in traskObject) {
+      loadedTask.push({ id: taskKey, text: traskObject[taskKey].text });
     }
-    setIsLoading(false);
-  };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    setTasks(loadedTask);
+  };
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
 
+  //* HTTP HOOK
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://react-test-98851-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
+
+  //* useEffect
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  //* return
   return (
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
