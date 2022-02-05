b794469cfcfbe43daad050eb645c2b6783450c17
ejyang <trustnloveu@gmail.com>
2022-02-05 18:26:55 +0900

    useState & useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,48 @@
+import { useRef, useState } from "react";
+
+/*
+  To Handle User Input in Form
+
+  1. Using 'State'
+    - You'd like to check every key-stroke of user
+  2. Using 'Ref'
+    - It can be a good way in case you need to evaludate user input when the user submit the form
+*/
+
 const SimpleInput = (props) => {
+  //* state
+  const [enteredName, setEnteredName] = useState("");
+
+  //* ref
+  const nameInputRef = useRef();
+
+  //* method
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); //! To avoid sending HTTP request to the server with current url
+
+    // const enteredValue = nameInputRef.current.value;
+
+    // To initialize form
+    // nameInputRef.current.value = '' //! Not ideal and recommanded. Don't manipulate the DOM, but, let React handler do.
+    setEnteredName("");
+  };
+
+  //* return
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
