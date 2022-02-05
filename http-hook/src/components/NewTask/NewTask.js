a64d0ddb2ef5f46ef044caaf55f7502872f3b650
ejyang <trustnloveu@gmail.com>
2022-02-05 17:59:53 +0900

    POST request

--- http-hook/src/components/NewTask/NewTask.js
+++ http-hook/src/components/NewTask/NewTask.js
@@ -1,43 +1,37 @@
-import { useState } from "react";
+import { useEffect } from "react";
+
+import useHttp from "../../hooks/use-http";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  //* HTTP Hook
+  const { isLoading, error, sendRequest: postTask } = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-test-98851-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+  //* method
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    props.onAddTask(createdTask);
   };
 
+  const enterTaskHandler = (taskText) => {
+    postTask(
+      {
+        url: "https://react-test-98851-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  //* return
   return (
     <Section>
       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
