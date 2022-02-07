8a7c8dd819c729648716206bb7aa037d42ceb15d
ejyang <trustnloveu@gmail.com>
2022-02-08 00:08:53 +0900

    conditonal view with authSlice

--- redux-starter/src/App.js
+++ redux-starter/src/App.js
@@ -1,14 +1,21 @@
+import { useSelector } from "react-redux";
+
 import Header from "./components/Header";
 import Auth from "./components/Auth";
 import Counter from "./components/CounterSlice";
+import UserProfile from "./components/UserProfile";
 // import Counter from "./components/Counter";
 // import Counter from "./components/CounterClass";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  //* return
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </>
   );
