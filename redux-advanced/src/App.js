cfb8d426379dee339abc68a1eb8ae6e0b3a2fed8
ejyang <trustnloveu@gmail.com>
2022-02-12 16:37:32 +0900

    Update Cart Data of Firebase

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -1,60 +1,25 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Axios } from "./http/axios";
-
-import { cartActions } from "./store/cart-slice";
-import { productActions } from "./store/product-slice";
+import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { Axios } from "./http/axios";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
-  // const cart = useSelector((state) => state.cart);
-
-  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
 
   //* useEffect
   useEffect(() => {
-    //* getProducts
-    const getProducts = async () => {
-      const { data: products } = await Axios.get("/product.json");
-
-      if (products) {
-        const productList = [];
-
-        for (const key in products) {
-          productList.push({
-            id: key,
-            price: products[key].price,
-            title: products[key].title,
-            description: products[key].description,
-          });
-        }
-
-        dispatch(productActions.setProducts(productList));
-      }
+    const setCart = async () => {
+      await Axios.put("/cart.json", {
+        data: cart,
+      });
     };
 
-    //* getCart
-    const getCart = async () => {
-      const { data: cart } = await Axios.get("/cart.json");
-
-      if (cart) {
-        dispatch(
-          cartActions.replaceCart({
-            totalQuantity: cart.totalQuantity,
-            items: cart.items,
-          })
-        );
-      }
-    };
-
-    //* Execute
-    getProducts();
-    getCart();
-  }, [dispatch]);
+    setCart();
+  }, [cart]);
 
   //* return
   return (
--- redux-advanced/src/components/Shop/Products.js
+++ redux-advanced/src/components/Shop/Products.js
@@ -1,9 +1,42 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { productActions } from "../../store/product-slice";
+import { Axios } from "../../http/axios";
+
 import ProductItem from "./ProductItem";
+
 import classes from "./Products.module.css";
 
 const Products = (props) => {
   const products = useSelector((state) => state.product.products);
+
+  const dispatch = useDispatch();
+
+  //* useEffect
+  useEffect(() => {
+    //* getProducts
+    const getProducts = async () => {
+      const { data: products } = await Axios.get("/product.json");
+
+      if (products) {
+        const productList = [];
+
+        for (const key in products) {
+          productList.push({
+            id: key,
+            price: products[key].price,
+            title: products[key].title,
+            description: products[key].description,
+          });
+        }
+
+        dispatch(productActions.setProducts(productList));
+      }
+    };
+
+    //* Execute
+    getProducts();
+  }, [dispatch]);
 
   //* return
   return (
