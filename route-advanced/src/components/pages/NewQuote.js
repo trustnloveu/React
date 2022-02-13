91386a5eecea9f8e499351970f4efe1146d5fec7
ejyang <trustnloveu@gmail.com>
2022-02-14 00:44:58 +0900

    useHistory

--- route-advanced/src/components/pages/NewQuote.js
+++ route-advanced/src/components/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    history.push("/quotes"); //! = location.href
   };
 
   //* return
